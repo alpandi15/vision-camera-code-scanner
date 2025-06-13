1c7d58db1ebca88c3ceb35ab2d64f2e339c37cd5
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2025-06-13 13:56:47 +0700

    update scanner

--- android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPluginPackage.java
+++ android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPluginPackage.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,6 +16,7 @@ public class VisionCameraCodeScannerPluginPackage implements ReactPackage {
   @org.jetbrains.annotations.NotNull
   @Override
   public List<NativeModule> createNativeModules(@NonNull @org.jetbrains.annotations.NotNull ReactApplicationContext reactContext) {
+    FrameProcessorPlugin.register(new VisionCameraCodeScannerPlugin());
     return Collections.emptyList();
   }
 
